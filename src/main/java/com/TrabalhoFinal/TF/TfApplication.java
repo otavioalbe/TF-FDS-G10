fdf8a01ac250e687d8f63f939eb8c92d61ce4e61
Otávio De Carli Albuquerque <otavioalbe@gmail.com>
2023-06-14 18:11:24 -0300

    1.0

---
+++ src/main/java/com/TrabalhoFinal/TF/TfApplication.java
@@ -0,0 +1,13 @@
+package com.TrabalhoFinal.TF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfApplication.class, args);
+	}
+
+}
